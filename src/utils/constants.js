364c432dc99fd636202b2b10b29aac717713b88e
Amardeep001 <myamardeepsingh001@gmail.com>
2024-08-07 05:45:10 +0000

    url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://1d4d-20-244-107-209.ngrok-free.app/";
+export const API_URL = "http://20.244.107.209:8004/";
 // http://20.244.107.209:8004/
 // export const API_URL = "https://9e7f-20-244-107-209.ngrok-free.app/";
 // https://420a-2401-4900-4469-ef44-c59e-2516-9a31-b56a.ngrok-free.app
